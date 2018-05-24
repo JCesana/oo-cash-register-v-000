f489eaa855c573e004717d9fc0ec40d474cb4bb7
Josh Cesana <joshuacesana@gmail.com>
2018-05-24 22:44:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   
   def apply_discount
     if @discount != 0 
-      @total = @total * (1.0 - @discount)
+      @total = @total * (1.0 - (@discount * .001) )
       @total
     else 
       @total
